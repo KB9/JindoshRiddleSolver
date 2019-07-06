cfb7ab6a6bcd1e182195744008a7acec426290a6
KB9 <kavanbickerstaff@googlemail.com>
2019-07-06 14:43:36 +0100

    Jindosh riddle included in Jindosh riddle page

--- client/src/pages/JindoshRiddlePage/JindoshRiddlePage.js
+++ client/src/pages/JindoshRiddlePage/JindoshRiddlePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import JindoshRiddle from "./components/JindoshRiddle";
+
 import "./JindoshRiddlePage.css";
 
 class JindoshRiddlePage extends React.Component {
   render() {
     return (
       <div className="jindosh-riddle-page">
-        <p style={{color: "white"}}>Coming soon!</p>
+        <JindoshRiddle />
       </div>
     );
   }
