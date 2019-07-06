16af74a3abc85131622b702bf7bd1d4eb202d65b
KB9 <kavanbickerstaff@googlemail.com>
2019-07-06 14:04:13 +0100

    Jindosh riddle page component added to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import JindoshRiddlePage from "pages/JindoshRiddlePage";
+
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Coming soon!</h1>
+        <JindoshRiddlePage />
       </div>
     );
   }
